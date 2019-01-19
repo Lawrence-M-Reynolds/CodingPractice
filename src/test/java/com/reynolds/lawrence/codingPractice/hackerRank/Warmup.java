e84940211d1c2b4294473d67271ded325836d016
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2019-01-19 15:47:51 +0000

    Enabling and fixing old tests.

--- src/test/java/com/reynolds/lawrence/codingPractice/hackerRank/Warmup.java
+++ src/test/java/com/reynolds/lawrence/codingPractice/hackerRank/Warmup.java
@@ -1,9 +1,11 @@
 package com.reynolds.lawrence.codingPractice.hackerRank;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -15,8 +17,11 @@ public class Warmup {
     	int posCount = 0;
     	int negCount = 0;
     	
+		final URL fileURL = this.getClass().getClassLoader().getResource("hackerRank/warmup/inputFile.txt");
+		final File file = new File(fileURL.getFile());
+    	
         try (
-        		FileReader r = new FileReader("resources/inputFile.txt");
+        		FileReader r = new FileReader(file);
         		BufferedReader br = new BufferedReader(r)
         	){
         	n = Integer.parseInt(br.readLine());
@@ -54,8 +59,11 @@ public class Warmup {
     
     @Test
     public void timeConversion(){
+		final URL fileURL = this.getClass().getClassLoader().getResource("hackerRank/warmup/timeConversionInput.txt");
+		final File file = new File(fileURL.getFile());
+		
     	try (
-    			FileReader fr = new FileReader("resources/warmup/timeConversionInput.txt");
+    			FileReader fr = new FileReader(file);
     			BufferedReader br = new BufferedReader(fr);
     			){
     		String inputTime = null;
