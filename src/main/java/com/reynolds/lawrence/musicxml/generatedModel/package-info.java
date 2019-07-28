affd2abfc921b14bf99a8d3bce9d662e773e9c03
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2019-07-28 17:47:09 +0100

    Generated data model from musicxml.xsd.

---
+++ src/main/java/com/reynolds/lawrence/musicxml/generatedModel/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Generated code using the following command:
+ * <pre>
+ *     p://www.musicxml.org/xsd/musicxml.xsd -d .\src\main\java -b .\src\main\resources\musicxml-3.1\LR_CustomBindings.xjb -p com.reynolds.lawrence.musicxml.generatedModel
+ * </pre>
+ */
+package com.reynolds.lawrence.musicxml.generatedModel;
